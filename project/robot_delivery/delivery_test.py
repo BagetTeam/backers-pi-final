5c130da69b1eccc0b602246d06707807b4414961
Ewen <ewengue1432@gmail.com>
2025-11-12 13:25:55 -0500

    delivery test cleanup

--- project/robot_delivery/delivery_test.py
+++ project/robot_delivery/delivery_test.py
@@ -1,5 +1,6 @@
 from time import sleep
 import delivery_system as deliv_sys
+from utils.brick import TouchSensor, Motor
 
 ROTATE_BUTTON = TouchSensor(1);
 MOTOR = Motor("A")
@@ -25,44 +26,3 @@ class DeliveryTest:
             self.delivery_system.push(PUSH_POWER, PUSH_TIME)
             while not ROTATE_BUTTON.is_pressed():
                 sleep(0.1)
-
-
-# power const
-ROTATE_POWER = 10
-ROTATE_TIME = 1.0
-PUSH_POWER = 50
-
-CUR_MODE = "ROTATE"  # ROTATE or PUSH
-
-wait_ready_sensors(True)
-print("Done waiting")
-
-def main() :
-    try:
-        # Main loop: runs indefinitely
-        while True: 
-            if CUR_MODE == "PUSH":
-                while ROTATE_BUTTON.is_pressed():
-                    sleep(0.1)
-                delivery_system.push(PUSH_POWER, ROTATE_TIME/2)
-                while not ROTATE_BUTTON.is_pressed():
-                    sleep(0.1)
-                    
-            elif CUR_MODE == "ROTATE":
-                while ROTATE_BUTTON.is_pressed():
-                    sleep(0.1)
-                delivery_system.rotate(ROTATE_POWER, ROTATE_TIME)
-                while not ROTATE_BUTTON.is_pressed():
-                    sleep(0.1)
-            
-    except BaseException:
-        pass
-    finally:
-        print("Done testing")
-        reset_brick()  # Turn off everything on the brick's hardware, and reset it
-        exit()
-
-if __name__ == "__main__":
-    main()
-
-
