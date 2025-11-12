7e01901f880ab56c968d365888f255131b52aa22
Benjamin <benjamincf01@gmail.com>
2025-11-12 15:06:26 -0500

    delivery system fixed

--- project/robot_delivery/delivery_system.py
+++ project/robot_delivery/delivery_system.py
@@ -2,9 +2,13 @@ from time import sleep
 from utils.brick import Motor, wait_ready_sensors
 
 class DeliverySystem:
+    START_POSITION = 0
+    PUSHED_POSITION = 180
+
     def __init__(self, motor: Motor):
         wait_ready_sensors()
         self.motor = motor
+        self.motor.reset_encoder()  # Ensure we start from position 0
 
     # windmill-like delivery system
     def rotate(self, power: int = 10, duration: float = 1.0):
@@ -12,12 +16,10 @@ class DeliverySystem:
         sleep(duration)
         self.motor.set_power(0)
 
-
-
-    # piston-like delivery system
-    def push(self, power: int = 50, duration: float = 0.5):
-        self.motor.set_power(power)
-        sleep(duration)
-        self.motor.set_power(-power)
-        sleep(duration)
-        self.motor.set_power(0)
+    # piston-like delivery system - pushes 180 degrees out and back
+    def push(self, power: int = 50, dps: int = 100):
+        self.motor.set_limits(power=power, dps=dps)
+        self.motor.set_position(self.PUSHED_POSITION)
+        self.motor.wait_is_stopped()  # Wait for motor to reach 180 degrees
+        self.motor.set_position(self.START_POSITION)
+        self.motor.wait_is_stopped()  # Wait for motor to return to 0 degrees
