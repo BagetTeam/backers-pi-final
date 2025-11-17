432eb53c16ecc3010350b6fa273f3f4b06693473
Ewen <ewengue1432@gmail.com>
2025-11-17 14:04:34 -0500

    line tracking

--- project/linetracking_system/linetracker.py
+++ project/linetracking_system/linetracker.py
@@ -4,7 +4,7 @@ from color_sensor.color_sensor import ColorSensor
 from time import sleep
 
 class LineTracker(RobotMovement):
-    def __init__(self, left_motor, right_motor, color_sensor: ColorSensor):
+    def __init__(self, left_motor: Motor, right_motor: Motor, color_sensor: ColorSensor):
         super().__init__(left_motor, right_motor)
         self.color_sensor = color_sensor
         self.isLeft = False
@@ -24,13 +24,16 @@ class LineTracker(RobotMovement):
         # use it to calibrate the middle brightness value.
         # adjust
         # convert color sensor value to a range between -100 to 100
+
+        previous_color = -1
         while True:
-            color = self.color_sensor.get_rgb()
-            if color[0] < 50 and color[1] < 50 and color[2] < 50: #black
-                if self.isLeft:
-                    self.adjust_speed(base_power, base_power + 100)
-                    self.isLeft = False
-                else:
-                    self.adjust_speed(base_power + 100, base_power)
-                    self.isLeft = True
+            color = self.color_sensor.get_color_detected()
+            if color == "BLACK":
+                self.adjust_speed(0, 0)
+                self.change_relative_angle(10, -10)
+                sleep(0.1)
+                previous_color = 0
+            elif color == "WHITE" and previous_color != 1:
+                self.adjust_speed(base_power, base_power + 5)
+                previous_color = 1
             sleep(0.01)
