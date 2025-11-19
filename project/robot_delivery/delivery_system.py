bcd8cd969b16fb73db0ea0136b8f601fa5452f27
mingli202 <vincentmingli@gmail.com>
2025-11-19 10:02:20 -0500

    updated stuff

--- project/robot_delivery/delivery_system.py
+++ project/robot_delivery/delivery_system.py
@@ -1,4 +1,5 @@
 from time import sleep
+from color_sensor.color_sensor import ColorSensor
 from utils.brick import Motor, wait_ready_sensors
 
 
@@ -6,15 +7,27 @@ class DeliverySystem:
     delivery_motor: Motor
     left_motor: Motor
     right_motor: Motor
+    sensor: ColorSensor
+
     is_active: bool = True
     deg: int = 0
     has_first_been_pushed = False
 
-    def __init__(self, motor: Motor, left_motor: Motor, right_motor: Motor):
+    def __init__(
+        self, motor: Motor, left_motor: Motor, right_motor: Motor, sensor: ColorSensor
+    ):
         self.delivery_motor = motor
         self.left_motor = left_motor
         self.right_motor = right_motor
+        self.sensor = sensor
         self.delivery_motor.reset_encoder()  # Ensure we start from position 0
+
+    def deliver(self):
+        color = self.sensor.get_color_detected()
+
+        if color == "GREEN":
+            self.move_back()
+            self.push()
 
     def move_back(self, power: int = 50, duration: float = 0):
         self.right_motor.set_position(-100)
