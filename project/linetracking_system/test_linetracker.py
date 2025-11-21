77e1999a2b7a74249c2e5b52b01e299b591488b0
mingli202 <vincentmingli@gmail.com>
2025-11-21 10:32:48 -0500

    peak linetracking

--- project/linetracking_system/test_linetracker.py
+++ project/linetracking_system/test_linetracker.py
@@ -1,8 +1,12 @@
 from linetracking_system.linetracker import LineTracker
 
+
 class LineTrackingTest:
     def __init__(self, linetracking: LineTracker):
         self.linetracking = linetracking
-    
-    def test(self, base_power, correction_factor):
-        self.linetracking.follow_line3(base_power=base_power, correction_factor=correction_factor)
+
+    def test(self, base_power: int, correction_factor: int):
+        # self.linetracking.follow_line3(
+        #     base_power=base_power, correction_factor=correction_factor
+        # )
+        self.linetracking.follow_line2()
--- project/main.py
+++ project/main.py
@@ -1,36 +1,37 @@
 from color_sensor.color_sensor import ColorSensor
 from utils.brick import (
+    EV3GyroSensor,
     Motor,
     TouchSensor,
     EV3ColorSensor,
     reset_brick,
-    configure_ports,
     wait_ready_sensors,
 )
-from robot_movement import robot_movement_test as robot_move_test
 from linetracking_system import linetracker, test_linetracker
 
 TOUCH1 = TouchSensor(1)
 COLOR = EV3ColorSensor(3)
+GYRO = EV3GyroSensor(4)
 MOTOR1 = Motor("A")
 MOTOR2 = Motor("D")
 wait_ready_sensors(True)
 
 COLOR_SENSOR = ColorSensor(COLOR)
+line_tracker = linetracker.LineTracker(MOTOR1, MOTOR2, COLOR_SENSOR, GYRO)
+line_tracker_test = test_linetracker.LineTrackingTest(line_tracker)
 
 
 def main():
     # movement_test = robot_move_test.MovementTest(TOUCH1, TOUCH2, MOTOR1, MOTOR2)
-    line_tracker = linetracker.LineTracker(MOTOR1, MOTOR2, COLOR_SENSOR)
-    line_tracker_test = test_linetracker.LineTrackingTest(line_tracker)
     try:
         line_tracker_test.test(10, 10)
         # movement_test.corner_turning_test(TURNING_POWER=25)
     except BaseException:
-        print("WHYYYYYYYYY")
+        print("WHYYYYYYYYY hello world")
         pass
     finally:
         print("Done testing")
+        COLOR_SENSOR.dispose()
         reset_brick()  # Turn off everything on the brick's hardware, and reset it
         exit()
 
